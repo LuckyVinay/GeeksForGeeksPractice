e5f69b5ee0e2c0629028516ca37906d739ee1e1f
Vinay Chaudhary <47312533+LuckyVinay@users.noreply.github.com>
2021-07-17 03:55:14 +0530

    Create Find difference between sum of diagonals.cpp

---
+++ Find difference between sum of diagonals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int diagonalSumDifference(int N, vector<vector<int>> Grid) {
+        // code here
+        int sum =0 , sum1 = 0;
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                if(i==j)
+                {
+                    sum = sum + Grid[i][j];
+                }
+                if(i==N-j-1)
+                {
+                    sum1 = sum1 + Grid[i][j];
+                }
+            }
+        }
+        return abs(sum - sum1);
+    }
+};
