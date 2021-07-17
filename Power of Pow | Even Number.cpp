ffef638875c5a4261fc344e49b3a9c072d072156
Vinay Chaudhary <47312533+LuckyVinay@users.noreply.github.com>
2021-07-18 05:26:38 +0530

    Create Power of Pow | Even Number.cpp

---
+++ Power of Pow | Even Number.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+	public:
+		long long int sum_of_square_evenNumbers(long long int n)
+		{
+		    // Code here
+		    long long int sum = 0;
+		    for(int i=1; i<=n;i++)
+		    {
+		        sum = sum + pow(i*2,2);
+		    }
+		    return abs (sum);
+		}
+};
